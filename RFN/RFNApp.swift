00e0177f42f4b5331b4e10d7f7bb39ed5473506b
Joseph DeWeese <rangerboy901@icloud.com>
2022-09-17 10:48:47 -0500

    Initial Commit

---
+++ RFN/RFNApp.swift
@@ -0,0 +1,17 @@
+//
+//  RFNApp.swift
+//  RFN
+//
+//  Created by Joseph Wil;liam DeWeese on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct RFNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
